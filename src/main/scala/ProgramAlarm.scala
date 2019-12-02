b49dcd54c3fc2dbee96f5f033a5efbb3fa6647a4
jesus <jesusmluque@gmail.com>
2019-12-03 00:06:44 +0100

    Adding error control when the program is not correct

--- src/main/scala/ProgramAlarm.scala
+++ src/main/scala/ProgramAlarm.scala
@@ -2,16 +2,17 @@ import scala.annotation.tailrec
 
 object ProgramAlarm {
 
-  def execute(program: Vector[Int]):Vector[Int] = {
+  def execute(program: Vector[Int]):Either[String, Vector[Int]] = {
     @tailrec
-    def executeWithIndex(index: Int, program: Vector[Int]):Vector[Int] = {
+    def executeWithIndex(index: Int, program: Vector[Int]):Either[String, Vector[Int]] = {
       if (index >= program.size)
-        program
+        Right(program)
       else
         program(index) match {
           case 1 => executeWithIndex(index + 4, program.updated(program(index + 3), program(program(index + 1)) + program(program(index + 2))))
           case 2 => executeWithIndex(index + 4, program.updated(program(index + 3), program(program(index + 1)) * program(program(index + 2))))
-          case 99 => program
+          case 99 => Right(program)
+          case _ => Left("Error")
         }
     }
     executeWithIndex(0, program)
--- src/test/scala/ProgramAlarmTest.scala
+++ src/test/scala/ProgramAlarmTest.scala
@@ -5,34 +5,38 @@ import scala.io.Source
 class ProgramAlarmTest extends FlatSpec {
 
   "A program 1,9,10,3,2,3,11,0,99,30,40,50 " should "becomes 3500,9,10,70,2,3,11,0,99,30,40,50" in {
-    assert(ProgramAlarm.execute(Vector(1,9,10,3,2,3,11,0,99,30,40,50)) === Vector(3500,9,10,70,2,3,11,0,99,30,40,50))
+    assert(ProgramAlarm.execute(Vector(1,9,10,3,2,3,11,0,99,30,40,50)) === Right(Vector(3500,9,10,70,2,3,11,0,99,30,40,50)))
   }
 
   "A program 1,0,0,0,99 " should "becomes 2,0,0,0,99" in {
-    assert(ProgramAlarm.execute(Vector(1,0,0,0,99)) === Vector(2,0,0,0,99))
+    assert(ProgramAlarm.execute(Vector(1,0,0,0,99)) === Right(Vector(2,0,0,0,99)))
   }
 
   "A program 2,3,0,3,99 " should "becomes 2,3,0,6,99" in {
-    assert(ProgramAlarm.execute(Vector(2,3,0,3,99)) === Vector(2,3,0,6,99))
+    assert(ProgramAlarm.execute(Vector(2,3,0,3,99)) === Right(Vector(2,3,0,6,99)))
   }
 
   "A program 2,4,4,5,99,0 " should "becomes 2,4,4,5,99,9801" in {
-    assert(ProgramAlarm.execute(Vector(2,4,4,5,99,0)) === Vector(2,4,4,5,99,9801))
+    assert(ProgramAlarm.execute(Vector(2,4,4,5,99,0)) === Right(Vector(2,4,4,5,99,9801)))
   }
 
   "A program 1,1,1,4,99,5,6,0,99 " should "becomes 30,1,1,4,2,5,6,0,99" in {
-    assert(ProgramAlarm.execute(Vector(1,1,1,4,99,5,6,0,99)) === Vector(30,1,1,4,2,5,6,0,99))
+    assert(ProgramAlarm.execute(Vector(1,1,1,4,99,5,6,0,99)) === Right(Vector(30,1,1,4,2,5,6,0,99)))
+  }
+
+  "A program 3,1,1,4,99,5,6,0,99 " should "rise an error" in {
+    assert(ProgramAlarm.execute(Vector(3,1,1,4,99,5,6,0,99)) === Left("Error"))
   }
 
   "A program from file " should "get index 0 equal to  3850704" in {
     val program = Source.fromResource("ProgramAlarm.txt").getLines.flatMap(_.toString.split(",").map(_.toInt)).toVector
     val program1202 = program.updated(1, 12).updated(2, 2)
-    assert(ProgramAlarm.execute(program1202)(0) === 3850704)
+    assert(ProgramAlarm.execute(program1202).right.get(0) === 3850704)
   }
 
   "A program from file " should "get index 0 equal to  19690720" in {
     val program = Source.fromResource("ProgramAlarm.txt").getLines.flatMap(_.toString.split(",").map(_.toInt)).toVector
     val program1202 = program.updated(1, 67).updated(2, 18)
-    assert(ProgramAlarm.execute(program1202)(0) === 19690720)
+    assert(ProgramAlarm.execute(program1202).right.get(0) === 19690720)
   }
 }
