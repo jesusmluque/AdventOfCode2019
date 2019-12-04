118135e1034ab63b5f0ffb2477236d50850d6986
jesus <jesusmluque@gmail.com>
2019-12-04 16:07:34 +0100

    Refactor Secure Container

--- src/main/scala/SecureContainer.scala
+++ src/main/scala/SecureContainer.scala
@@ -1,41 +1,34 @@
 object SecureContainer {
 
-  def count(from: Int, to: Int) = {
-    (from to to).count(checkNumber(false))
+  type Number = Array[String]
+
+  def count(from: Int, to: Int) = (from to to).count(checkNumber(false))
+
+  def countStrict(from: Int, to: Int) = (from to to).count(checkNumber(true))
+
+  private def checkNumber(strict: Boolean)(num: Int): Boolean = {
+    val numS: Array[String] = getNumber(num)
+    checkConsecutiveDigits(numS) && checkExistAPair(numS, strict)
   }
 
-  def countStrict(from: Int, to: Int) = {
-    (from to to).count(checkNumber(true))
-  }
+  private def checkExistAPair(num: Number, strict: Boolean) = num.foldLeft(List[String]()) { (acc, n) =>
+    if (acc.nonEmpty && acc.head.head.toString == n)
+      acc.head ++ n :: acc.tail
+    else
+      n :: acc
+  }.count(r => (r.length == 2 && strict) || (!strict && r.length >= 2)) > 0
 
-  def checkNumber(strict: Boolean)(num: Int): Boolean = {
-    checkConsecutiveDigits(num) && checkExistAPair(num, strict)
-  }
-
-  private def checkExistAPair(num: Int, strict: Boolean) = {
-    val str = num.toString.split("")
-    val res = str.foldLeft(List[String]()) { (acc, n) =>
-      if (acc.nonEmpty && acc.head.head.toString == n)
-        acc.head ++ n :: acc.tail
-      else
-        n :: acc
-    }.count(r => (r.length == 2 && strict) || (!strict && r.length >= 2))
-    res > 0
-  }
-
-  private def checkConsecutiveDigits(num: Int) = {
-    val str = num.toString.split("")
-    val res = str.foldLeft((0, true)) { (acc, d) =>
-      if (!acc._2)
-        acc
-      else if (d.toInt > acc._1)
-        (d.toInt, true)
-      else if (d.toInt < acc._1)
-        (acc._1, false)
-      else
-        acc
+  private def checkConsecutiveDigits(num: Number) = num.foldLeft((0, true)) { (acc, d) =>
+    acc match {
+      case (_, false) => acc
+      case (n, _) if d.toInt > n => (d.toInt, true)
+      case (n, _) if d.toInt < n => (acc._1, false)
+      case _ => acc
     }
-    res._2
+  }._2
 
+  private def getNumber(n: Int) = {
+    val numS = n.toString.split("")
+    numS
   }
 }
