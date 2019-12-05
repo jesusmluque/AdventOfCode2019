9ad1d0c250105a919b71df283a72040974d23771
jesus <jesusmluque@gmail.com>
2019-12-05 20:20:32 +0100

    Refactor

--- src/main/scala/SecureContainer.scala
+++ src/main/scala/SecureContainer.scala
@@ -1,24 +1,24 @@
 object SecureContainer {
 
-  type Number = Array[String]
+  type Digits = Array[String]
 
-  def count(from: Int, to: Int) = (from to to).count(checkNumber(false))
+  def count(from: Int, to: Int): Int = (from to to).count(checkNumber(strict = false))
 
-  def countStrict(from: Int, to: Int) = (from to to).count(checkNumber(true))
+  def countStrict(from: Int, to: Int): Int = (from to to).count(checkNumber(strict = true))
 
   private def checkNumber(strict: Boolean)(num: Int): Boolean = {
-    val numS: Array[String] = getNumber(num)
+    val numS: Digits = getDigits(num)
     checkConsecutiveDigits(numS) && checkExistAPair(numS, strict)
   }
 
-  private def checkExistAPair(num: Number, strict: Boolean) = num.foldLeft(List[String]()) { (acc, n) =>
+  private def checkExistAPair(num: Digits, strict: Boolean) = num.foldLeft(List[String]()) { (acc, n) =>
     if (acc.nonEmpty && acc.head.head.toString == n)
       acc.head ++ n :: acc.tail
     else
       n :: acc
   }.count(r => (r.length == 2 && strict) || (!strict && r.length >= 2)) > 0
 
-  private def checkConsecutiveDigits(num: Number) = num.foldLeft((0, true)) { (acc, d) =>
+  private def checkConsecutiveDigits(num: Digits) = num.foldLeft((0, true)) { (acc, d) =>
     acc match {
       case (_, false) => acc
       case (n, _) if d.toInt > n => (d.toInt, true)
@@ -27,8 +27,5 @@ object SecureContainer {
     }
   }._2
 
-  private def getNumber(n: Int) = {
-    val numS = n.toString.split("")
-    numS
-  }
+  private def getDigits(n: Int): Digits = n.toString.split("")
 }
